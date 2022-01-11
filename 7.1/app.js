c8dbbd51ca5ddf3b5691481d9eb465c045c56252
yonin7 <yonin789@walla.com>
2022-01-11 18:48:25 +0200

    7.1 ex

---
+++ 7.1/app.js
@@ -0,0 +1,42 @@
+const request = require('request');
+const axios = require('axios');
+const got = require('got');
+
+const url = `https://api.fbi.gov/wanted/v1/list`;
+
+// const data = async (address, callback) => {
+//   request({ url: url, json: true }, (error, response) => {
+//     if (error) {
+//       callback();
+//     } else if (response.body.error) {
+//       console.log('ggd');
+//     } else {
+//       await console.log(response.data);
+//       const data = response.body.current;
+//     }
+//   });
+// };
+// data(url);
+
+request(url, { json: true }, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(body.items[0]);
+});
+axios
+  .get(url)
+  .then((response) => {
+    console.log(response.data.items[0]);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+got(url, { json: true })
+  .then((response) => {
+    console.log(response.body.items[0]);
+  })
+  .catch((error) => {
+    console.log(error.response.body);
+  });
---
+++ 7.1/callbacks.js
@@ -0,0 +1,16 @@
+setTimeout(() => {
+  console.log('ddddd');
+}, 2000);
+
+const names = ['yoni', 'ohad', 'roi'];
+
+const data = (x, callback) => {
+  setTimeout(() => {
+    const data = '';
+
+    return data;
+  }, 2000);
+};
+data('dsad', (data) => {
+  console.log(data);
+});
