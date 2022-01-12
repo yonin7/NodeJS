9607568b09cdf1267ba69fb2784ca1aa9d9dbc53
yonin7 <yonin789@walla.com>
2022-01-12 11:24:31 +0200

    10.1 ex

---
+++ 11.1/src/app.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+// app.get('', (req, res) => {
+//   res.send('<h1>hello</h1>');
+// });
+app.get('/help', (req, res) => {
+  res.send('help page');
+});
+
+app.listen(3000, () => {
+  console.log('server is up on port 3000');
+});
