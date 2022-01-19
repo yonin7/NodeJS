c614a85eb5731201d3596ec6c0c5d9007eb9cc5a
yonin7 <yonin789@walla.com>
2022-01-19 08:05:49 +0200

    18.1 ex

--- 18.1/src/index.js
+++ 18.1/src/index.js
@@ -42,7 +42,42 @@ app.get('/products/:id', (req, res) => {
       res.status(500).send();
     });
 });
+app.get('/actives', (req, res) => {
+  const ac = Product.find({ isActive: false });
+  console.log('req');
+  Product.find({ isActive: true })
+    .then((product) => {
+      if (!product) {
+        return res.status(404).send();
+      }
+      res.status(200).send(product);
+    })
+    .catch((err) => {
+      res.status(500).send('why');
+    });
+});
 
+app.get('/price/:min', (req, res) => {
+  const min = req.params.min;
+  console.log(min);
+  const max = req.params.max;
+  Product.aggregate({
+    $group: {
+      _id: null,
+      //   max: { $max: '$price' },
+      $min: { 'details.price': min },
+    },
+  })
+    .then((product) => {
+      if (!product) {
+        return res.status(404).send();
+      }
+      res.status(200).send(product);
+    })
+    .catch((err) => {
+      res.status(500).send();
+    });
+});
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
